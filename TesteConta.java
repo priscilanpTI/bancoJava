fdbb56d78e71a65ad51c324202e57105b619082b
Gustavo Alves Ramos <0001138233@senaimgaluno.com.br>
2026-04-13 16:21:04 -0300

    Adicionando os arquivos modificados

--- TesteConta.java
+++ TesteConta.java
@@ -1,15 +1,16 @@
 package teste;
 
+import banco.modelo.Conta;
 import banco.modelo.ContaCorente;
 import banco.modelo.ContaPoupanca;
 
 public class TesteConta {
     public static void main(String[] args) {
-        Conta conta1 = new ContaCorente("Priscila", 1000);
+        Conta conta1 = new Conta("Priscila", 1000);
         System.out.println("Dados da conta corrente: \nTitular: "+conta1.getTitular() + "\nSaldo: " + conta1.getSaldo());
 
-        ContaPoupanca cont2 = new ContaCorente("Priscila", 1000);
-        System.out.println("Dados da conta corrente: \nTitular: "+conta1.getTitular() + "\nSaldo: " + conta1.getSaldo());
+        ContaPoupanca cont2 = new ContaPoupanca("Priscila", 1000);
+        System.out.println("Dados da conta poupança : \nTitular: "+conta1.getTitular() + "\nSaldo: " + conta1.getSaldo());
 
     }
 }
