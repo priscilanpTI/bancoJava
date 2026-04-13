d5a33702eb81ca06d5728ccb53b2bac56acee7c0
David Fernandes <dfgamerwolf12@gmail.com>
2026-04-13 14:06:51 -0300

    Arquivos adicionados

--- src/banco/modelo/ContaPoupanca.java
+++
@@ -1,9 +0,0 @@
-package banco.modelo;
-
-public abstract class ContaPoupanca extends Conta {
-
-
-    public ContaPoupanca(String titular, double saldoInicial) {
-        super(titular, saldoInicial);
-    }
-}
